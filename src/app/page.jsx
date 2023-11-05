43f58e86f293958dc3b30eb262fba59f1b741413
Ham12-3 <mobolaji2309@gmail.com>
2023-11-05 10:34:59 +0000

    'update'

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import styles from "./homepage.module.css";
 
 export default function Home() {
   return <div>
-<Link>Hello</Link>
+<Link>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit quam architecto consequuntur quidem iure dolores error, expedita voluptates voluptate assumenda eos vel nesciunt autem corrupti cum temporibus a excepturi voluptatem?</Link>
 
   </div>;
 }
