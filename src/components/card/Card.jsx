0976931d512dc3855c67e3ea1d3c9e719ce5ad14
Ham12-3 <mobolaji2309@gmail.com>
2023-11-06 22:48:56 +0000

    'update'

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -8,18 +8,18 @@ const Card = () => {
   return (
     <div className={styles.container}>
       <div className={styles.imageContainer}>
-        <Image src="/p1.jpeg" alt="" fill />
+        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
       </div>
       <div className={styles.textContainer}>
         <div className={styles.detail}>
-          <span className={styles.date}>11.02.2023</span>
+          <span className={styles.date}>11.02.2023  - </span>
           <span className={styles.category}>CULTURE</span>
         </div>
         <Link href='/'>
         <h1>Lifestyle and Culture Insights</h1>
         </Link>
        
-        <p>
+        <p className={styles.desc}>
           You can also find fitness and exercise suggestions, pet care and
           heartwarming animal stories, gaming and hobbies, science and nature
           discoveries, and travel photography alongside financial planning and
