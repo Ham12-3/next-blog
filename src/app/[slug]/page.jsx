9f049d54b686fb3788e1cffd4e01e45ff542baf3
Ham12-3 <mobolaji2309@gmail.com>
2023-11-09 14:59:55 +0000

    'update'

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -9,9 +9,20 @@ const SinglePage = () => {
   return (
     <div className={styles.container}> 
     <div className={styles.infoContainer}>
-        <div className={styles.textContainer}></div>
+        <div className={styles.textContainer}>
+            <h1 className={styles.title}>Welcome to Anglaar Digital Agency</h1>
+            <div className={styles.user}>
+                <div className={styles.userImageContainer}>
+                    <Image src='/p1.jpeg' alt='' fill className={styles.avatar}/>
+                </div>
+                <div className={styles.userTextContainer}>
+                    <span className={styles.username}>Abdulhamid Sonaike</span>
+                    <span className={styles.date}>03.10.2023</span>
+                </div>
+            </div>
+        </div>
         <div className={styles.imageContainer}>
-            <Image src='' alt='' fill className={styles.image} />
+            <Image src='/p1.jpeg' alt='' fill className={styles.image} />
         </div>
     </div>
     <div className={styles.content}>
