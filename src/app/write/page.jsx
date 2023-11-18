df1226f08f4a6e43b4a39217ebb9adba5e0a8af4
Ham12-3 <mobolaji2309@gmail.com>
2023-11-18 23:26:03 +0000

    'update'

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -7,8 +7,23 @@ import Image from "next/image";
 
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.bubble.css";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const WritePage = () => {
+  const { status} = useSession()
+
+const router = useRouter()
+
+
+if(status=== 'loading') {
+  return <div className={styles.loading}>Loading...</div>
+} 
+
+if (status === 'authenticated') {
+  router.push('/')
+}
+
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState("");
 
